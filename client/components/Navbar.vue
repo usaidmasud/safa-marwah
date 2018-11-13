76bfaa1c18617cb399ba6f9b8a773076b2b50bdb
usaidmasud <masudsyarip808@gmail.com>
2018-11-13 08:39:08 +0700

    client

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,5 +1,9 @@
+
+
+
+
 <template>
-    <div>
+    <div class="container is-fluid">
         <nav class="navbar" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="https://bulma.io">
@@ -14,51 +18,49 @@
             </div>
 
             <div id="navbarBasicExample" class="navbar-menu">
+
                 <div class="navbar-start">
-                <a class="navbar-item">
-                    Home
-                </a>
+                    <nuxt-link exact-active-class="is-active" class="navbar-item" to="/">Home</nuxt-link>
+                    <nuxt-link exact-active-class="is-active" class="navbar-item" to="/shop">Shop</nuxt-link>
+                    <nuxt-link exact-active-class="is-active" class="navbar-item" to="/contact-us">Contact Us</nuxt-link>
+                    <nuxt-link exact-active-class="is-active" class="navbar-item" to="/about">About</nuxt-link>
 
-                <a class="navbar-item">
-                    Documentation
-                </a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                        More
+                        </a>
 
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                    More
-                    </a>
-
-                    <div class="navbar-dropdown">
-                    <a class="navbar-item">
-                        About
-                    </a>
-                    <a class="navbar-item">
-                        Jobs
-                    </a>
-                    <a class="navbar-item">
-                        Contact
-                    </a>
-                    <hr class="navbar-divider">
-                    <a class="navbar-item">
-                        Report an issue
-                    </a>
+                        <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            About
+                        </a>
+                        <a class="navbar-item">
+                            Jobs
+                        </a>
+                        <a class="navbar-item">
+                            Contact
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            Report an issue
+                        </a>
+                        </div>
                     </div>
-                </div>
                 </div>
 
                 <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
+                    <div class="navbar-item">
+                        <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light">
+                            Log in
+                        </a>
+                        </div>
                     </div>
                 </div>
-                </div>
             </div>
-            </nav>
+        </nav>
     </div>
 </template>
---
+++ client/layouts/app.vue
@@ -0,0 +1,20 @@
+<template>
+    
+    
+    <div>
+        <Navbar/>
+        <div class="container">
+            <nuxt/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Navbar from '~/components/Navbar'
+    
+    export default {
+        components : {
+            Navbar,
+        }
+    }
+</script>
